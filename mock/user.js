8379a0b7909a94e1c2e46c939ba8d908b09df0ee
test <test@qinghuan-2.local>
2019-12-06 17:52:29 +0800

    登录模拟数据

--- mock/user.js
+++ mock/user.js
@@ -1,138 +1,110 @@
 // 代码中会兼容本地 service mock 以及部署站点的静态数据
 export default {
   // 支持值为 Object 和 Array
-  'GET /api/currentUser': {
-    name: 'BiaoChenXuying',
-    // avatar: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
-    avatar: 'http://p61te2jup.bkt.clouddn.com/WechatIMG8.jpeg',
-    userid: '00000001',
-    email: 'antdesign@alipay.com',
-    signature: '海纳百川，有容乃大',
-    title: '交互专家',
-    group: 'BiaoChenXuying',
-    tags: [
-      {
-        key: '0',
-        label: '很有想法的',
-      },
-      {
-        key: '1',
-        label: '专注设计',
-      },
-      {
-        key: '2',
-        label: '辣~',
-      },
-      {
-        key: '3',
-        label: '大长腿',
-      },
-      {
-        key: '4',
-        label: '川妹子',
-      },
-      {
-        key: '5',
-        label: '海纳百川',
-      },
-    ],
-    notifyCount: 12,
-    country: 'China',
-    geographic: {
-      province: {
-        label: '浙江省',
-        key: '330000',
-      },
-      city: {
-        label: '杭州市',
-        key: '330100',
-      },
-    },
-    address: '西湖区工专路 77 号',
-    phone: '0752-268888888',
+  "GET /api/currentUser": {
+    code: 0,
+    currentAuthority: "BiaoChenXuYing",
+    data: {
+      avatar: "user",
+      create_time: "2019-12-01T13:45:06.534Z",
+      email: "admin@qq.com",
+      github_id: "",
+      id: 1,
+      img_url: "",
+      introduce:
+        "加班到天明，学习到昏厥!!! 微信公众号：【 BiaoChenXuYing 】，分享 WEB 全栈开发等相关的技术文章，热点资源，全栈程序员的成长之路。",
+      location: "user",
+      name: "BiaoChenXuYing",
+      password: "9fe9ae8e5cf24b47cb2806dc208520fd",
+      phone: "1380013800",
+      type: 0,
+      update_time: "2019-12-01T13:45:06.535Z",
+      __v: 0,
+      _id: "5de3c3e29fc4376866835add"
+    }
   },
   // GET POST 可省略
-  'GET /api/users': [
+  "GET /api/users": [
     {
-      key: '1',
-      name: 'John Brown',
+      key: "1",
+      name: "John Brown",
       age: 32,
-      address: 'New York No. 1 Lake Park',
+      address: "New York No. 1 Lake Park"
     },
     {
-      key: '2',
-      name: 'Jim Green',
+      key: "2",
+      name: "Jim Green",
       age: 42,
-      address: 'London No. 1 Lake Park',
+      address: "London No. 1 Lake Park"
     },
     {
-      key: '3',
-      name: 'Joe Black',
+      key: "3",
+      name: "Joe Black",
       age: 32,
-      address: 'Sidney No. 1 Lake Park',
-    },
+      address: "Sidney No. 1 Lake Park"
+    }
   ],
-  'POST /api/login/account': (req, res) => {
+  "POST /api/login/account": (req, res) => {
     const { password, userName, type } = req.body;
-    if (password === '888888' && userName === 'admin') {
+    if (password === "888888" && userName === "admin") {
       res.send({
-        status: 'ok',
+        status: "ok",
         type,
-        currentAuthority: 'admin',
+        currentAuthority: "admin"
       });
       return;
     }
-    if (password === '123456' && userName === 'user') {
+    if (password === "123456" && userName === "user") {
       res.send({
-        status: 'ok',
+        status: "ok",
         type,
-        currentAuthority: 'user',
+        currentAuthority: "user"
       });
       return;
     }
     res.send({
-      status: 'error',
+      status: "error",
       type,
-      currentAuthority: 'guest',
+      currentAuthority: "guest"
     });
   },
-  'POST /api/register': (req, res) => {
-    res.send({ status: 'ok', currentAuthority: 'user' });
+  "POST /api/register": (req, res) => {
+    res.send({ status: "ok", currentAuthority: "user" });
   },
-  'GET /api/500': (req, res) => {
+  "GET /api/500": (req, res) => {
     res.status(500).send({
       timestamp: 1513932555104,
       status: 500,
-      error: 'error',
-      message: 'error',
-      path: '/base/category/list',
+      error: "error",
+      message: "error",
+      path: "/base/category/list"
     });
   },
-  'GET /api/404': (req, res) => {
+  "GET /api/404": (req, res) => {
     res.status(404).send({
       timestamp: 1513932643431,
       status: 404,
-      error: 'Not Found',
-      message: 'No message available',
-      path: '/base/category/list/2121212',
+      error: "Not Found",
+      message: "No message available",
+      path: "/base/category/list/2121212"
     });
   },
-  'GET /api/403': (req, res) => {
+  "GET /api/403": (req, res) => {
     res.status(403).send({
       timestamp: 1513932555104,
       status: 403,
-      error: 'Unauthorized',
-      message: 'Unauthorized',
-      path: '/base/category/list',
+      error: "Unauthorized",
+      message: "Unauthorized",
+      path: "/base/category/list"
     });
   },
-  'GET /api/401': (req, res) => {
+  "GET /api/401": (req, res) => {
     res.status(401).send({
       timestamp: 1513932555104,
       status: 401,
-      error: 'Unauthorized',
-      message: 'Unauthorized',
-      path: '/base/category/list',
+      error: "Unauthorized",
+      message: "Unauthorized",
+      path: "/base/category/list"
     });
-  },
+  }
 };
